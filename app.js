ee0bba3503ab355c83e83c4e222adfe0744eee3e
Chase <Chase>
2015-01-25 20:14:05 -0500

    fixed some heroku port issues

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var app = express();
 var server = require('http').Server(app);
 exports.io = require('socket.io')(server);
 var gameServer = require('./server/server.js');
+var port = process.env.PORT || 3000;
 
 app.use('/client', express.static(__dirname + '/client'));
 app.use('/shared', express.static(__dirname + '/shared'));
 app.use('/resources', express.static(__dirname + '/resources'));
 app.use('/tools', express.static(__dirname + '/tools/tools.html'));
 
-server.listen(80);
+server.listen(port);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
